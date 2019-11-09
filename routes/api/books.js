6ef4c821f1b18cad733c9f3f12ad03e5efcbf2b6
Julian <cusco2891@gmail.com>
2019-11-08 19:58:50 -0500

    navtemplate:

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const bookController = require("../../controllers/bookController");
 
+// landing page
+router.route("/:id").post(bookController.create);
 
+// saved page
+router.route("/saved").get(bookController.findAllSaved);
+
+// delete call
+router.route("/delete/:id").delete(bookController.remove);
 
 module.exports = router;
